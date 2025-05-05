e18a2946656709192b190dda93405f78d56842c0
Pedro Henrique <95645963+Pedrohswd@users.noreply.github.com>
2025-05-05 09:56:20 -0300

    login e cadastro de usuário feitos (#2)

--- src/app/demo/components/auth/register/register.component.ts
+++ src/app/demo/components/auth/register/register.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
 	templateUrl: './register.component.html'
 })
 export class RegisterComponent {
-
-	constructor(private layoutService: LayoutService) {}
+	userForm: FormGroup
+	errorMessage: string = '';
+	loading = false;
+  
+	constructor(
+	  private userService: UserService,
+	  private router: Router,
+	  public layoutService: LayoutService,
+	  private fb: FormBuilder
+	) {
+	  this.userForm = this.fb.group({
+		name: ['', [Validators.required]],
+		email: ['', [Validators.required]],
+		password: ['', [Validators.required]],
+		date: ['', [Validators.required]],
+		role: ['USER']
+	  }, {
+	  });
+	}
+  
+	cadastre(): void {
+	  this.userService.register(this.userForm.value).subscribe({
+		next: () => {
+		  this.router.navigate(['./auth/login']);
+		},
+		error: (error) => {
+		  this.loading = false;
+		}
+	  });
+	}
+  
+	navigateTo(route: string) {
+	  this.router.navigate([route]);
+	}
 
 	get filledInput(): boolean {
 		return this.layoutService.config().inputStyle === 'filled';
